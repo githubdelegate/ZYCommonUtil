26d696e1b14b585448c3227539af1699785166b6
edz <xxxx@xxx.com>
2021-01-21 14:01:50 +0800

    add public

---
+++ Sources/ZYCommonUtil/UIApplication+Util.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by edz on 2021/1/14.
+//
+
+import Foundation
+import UIKit
+
+public extension UIApplication {
+    class func getTopViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return getTopViewController(base: nav.visibleViewController)
+
+        } else if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+            return getTopViewController(base: selected)
+
+        } else if let presented = base?.presentedViewController {
+            return getTopViewController(base: presented)
+        }
+        return base
+    }
+}
